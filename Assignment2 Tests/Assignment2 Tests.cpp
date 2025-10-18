5ac96d1084b5f0bd2ddf407f13b58285cd882f12
LakshayBhatia0910 <Lbhatia0874@conestogac.on.ca>
2025-10-18 19:38:59 -0400

    Initial commit - Cloned repo and added test project

---
+++ Assignment2 Tests/Assignment2 Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Assignment2Tests
+{
+	TEST_CLASS(Assignment2Tests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
